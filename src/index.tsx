8a21ea821469db24650546add271a4b2aae88a60
WJG <wjg12341234@163.com>
2022-04-30 15:43:57 +0800

    fix: BrowserRouter add basename

--- src/index.tsx
+++ src/index.tsx
@@ -23,7 +23,7 @@ root.render(
   <React.StrictMode>
     <Provider client={client}>
       <ChakraProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/NFT-Chocolate">
           <App />
         </BrowserRouter>
       </ChakraProvider>
