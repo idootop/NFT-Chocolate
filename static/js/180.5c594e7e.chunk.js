0329ce7c4a2436d801232e83e3d27a67ecf002ea
idootop <35302658+idootop@users.noreply.github.com>
2023-08-27 11:23:53 +0000

    Deploying to static-page from  @ 77ca4a9a8fdab3b3e9f8ed5f58547f665180185f 🚀

---
+++ static/js/180.5c594e7e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknft_chocolate=self.webpackChunknft_chocolate||[]).push([[180],{9180:(e,o,t)=>{t.r(o),t.d(o,{default:()=>c});var n=null;"undefined"!==typeof WebSocket?n=WebSocket:"undefined"!==typeof MozWebSocket?n=MozWebSocket:"undefined"!==typeof t.g?n=t.g.WebSocket||t.g.MozWebSocket:"undefined"!==typeof window?n=window.WebSocket||window.MozWebSocket:"undefined"!==typeof self&&(n=self.WebSocket||self.MozWebSocket);const c=n}}]);
+//# sourceMappingURL=180.5c594e7e.chunk.js.map
